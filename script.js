e84a9423d8cdd8d557bff3fe2e297fb71549d0cc
Chelsea Church <chelsea.ae.church@gmail.com>
2021-07-18 18:19:07 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('...connected')
+
+async function getQuotes() {
+    const api = 'https://type.fit/api/quotes'
+    try {
+        const response = await fetch(api)
+    } catch (error) {
+        console.error(error)
+    }
+}
