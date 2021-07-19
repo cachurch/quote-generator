d6ca84ffe101eba7f06a7816a3e7634ab5d7cc20
Chelsea Church <chelsea.ae.church@gmail.com>
2021-07-18 19:53:44 -0500

    added function to select a single quote

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-console.log('...connected')
+let apiQuotes = []
 
+//select single quote
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    console.log(quote)
+}
+
+//fetch quotes from API
 async function getQuotes() {
     const api = 'https://type.fit/api/quotes'
     try {
         const response = await fetch(api)
+        apiQuotes = await response.json()
+        newQuote()
     } catch (error) {
         console.error(error)
     }
 }
+
+getQuotes( )
